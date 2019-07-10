11f061d8ae03ee31e5d8a42ff31b977cadf0f265
Javier Goche <javier_warmer@hotmail.com>
2019-07-09 21:40:42 -0500

    TPM_Driver

--- source/SINI.c
+++ source/SINI.c
@@ -5,9 +5,16 @@
  *      Author: pmg
  */
 #include "SINI.h"
+#include "Driver_TPM.h"
 
 void SINI_vSystemInit ( void )
 {
 	/*!Add your Inits here*/
+
+	/* Configure tpm params with frequency 24kHZ */
+	TPM_Params ();
+
+	/* Initialize TPM module */
+	TPM_Initialize();
 
 }
